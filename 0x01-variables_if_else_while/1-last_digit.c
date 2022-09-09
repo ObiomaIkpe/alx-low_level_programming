58817867b2d0bd16093f49065e9bfa5d84a97541
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-09 06:47:25 -0700

    omo ooo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,6 @@
  * main - A function that compares different values
  *
  * Return: Always returns (0)
- *
  */
 
 
@@ -20,12 +19,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	if(last_digit > 5)
+	if (last_digit > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
 	
-	else if(last_digit == 0)
+	else if (last_digit == 0)
 	printf("Last digit of %d is %d and is 0\n", n, last_digit);
 
 	else
