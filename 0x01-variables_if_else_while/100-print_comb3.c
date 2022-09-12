c6154f2cbec1de448fb0251cae4586aa26a4755e
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-12 08:16:41 -0700

    number combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of code
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar ((i / 10) + '0');
+		putchar ((i % 10) + '0');
+		if (i != 99)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
