611b8e23b5287acb24272c166dfed5e355695195
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-12 10:11:25 -0700

    still on numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,40 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point of code
+ * main - print double digits
  *
- * Return: Always (0)
+ * Description: prints all double digits
+ * Return: Always return (0)
  */
-
 int main(void)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			for (k = '0'; k <= '9'; k++)
+			if (i < j)
 			{
-				if ((i < j) && (j < k) && (k <= '9'))
-				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-
-					if (( i + j + k) < 168)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+					break;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar('\n');
-
 	return (0);
 }
