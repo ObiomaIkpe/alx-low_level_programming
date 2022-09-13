2bb078b230df45f60c4531d8f4cc6ff7c5d3dd67
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-13 11:53:58 -0700

    sign

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -12,16 +12,17 @@ int _abs(int n)
 {
 	if (n < 0)
 	{
-		n = (-1) * n;
+		abs(n);
 		_putchar(n);
 	}
 	else if (n > 0)
 	{
-		
+		abs(n);
 		_putchar(n);
 	}
 	else
 	{
+		abs(n);
 		_putchar(n);
 	}
 	return (0);
