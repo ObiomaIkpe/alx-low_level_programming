c0820507b38e7d65772f159fceb199739df89af8
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-13 09:21:38 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "int _islower(int c)"
+
+/**
+ *int _islower(int c) - check for lowercase char 
+ *
+ *Description: checks for lower case characters
+ *Return: 1 if c is lowercase, else return 0
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else 
+		return (0)
+}
