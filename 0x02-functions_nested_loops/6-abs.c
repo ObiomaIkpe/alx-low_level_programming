56e37bbac60e0bd43074001cceb74e83f9f1252f
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-13 11:37:42 -0700

    sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * *_abs - finds absolute value of an integer
+ * @n: argument
+ * Description: Finds absolute value of an integer
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+		_putchar(n);
+	}
+	else if (n == 0)
+	{
+		n == 0;
+		_putchar(n);
+	}
+	else
+	{
+		_putchar(n);
+	}
+	return (0);
+}
+		
