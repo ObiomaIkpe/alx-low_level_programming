9205c9fc6df071932ad62fdc117d16f3aa2935ba
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-14 07:48:39 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print numbers from n to 98
+ * @n: required number
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if ( n <= 97)
+	{
+		while (n <= 97)
+		{
+			printf("%d", , n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 99)
+		{
+			printf("%d", n);
+			n--;
+		}
+	}
+	printf("98\n");
