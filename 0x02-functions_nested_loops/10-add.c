1a42467632daf84a629fa159b2acd927d3627e42
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-14 07:32:15 -0700

    sum of two

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * addition - add two numbers
+ * @i: first number
+ * @j: second number
+ * Return: returns the sum
+ */
+
+int add (int int)
+{
+	int sum = a + b;
+
+	return (sum);
+}
