647b3204c9e36ec919c37c845b883b1e889433ad
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-15 04:25:00 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- *
  * main - tests function that prints if integer is positive or negative
- * Description: Takes an arguement and checks for sign
  * Return: 0
- * */
+ */
+
 int main(void)
 {
 	int i;
