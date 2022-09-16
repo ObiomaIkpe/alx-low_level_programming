b641c628abe31f05cc330b09f0c6d6f18958d0fa
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-16 04:32:48 -0700

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num)
+	}
+	_putchar('\n');
+}
