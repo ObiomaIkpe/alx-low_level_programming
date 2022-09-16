36d8da152d741fbd4aaac8542ac89aba5ed14794
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-16 05:12:07 -0700

    five roe

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,20 @@
 void more_numbers(void)
 {
 	int num;
-	int count;
+	int count = 0;
 
-	count = 0;
 	while (count < 10)
 	{
-	for (num = 0; num <= 14; num++)
-	{
-		if (num > 9)
-			_putchar((num / 10 + '0');
+		for (num = 0; num <= 14); num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
 
-		_putchar((num % 10) + '0');
-	}
-	_putchar('\n');
-	count++;
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+		count++;
 	}
 }
+
+	
