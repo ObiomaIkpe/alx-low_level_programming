e8ae2fc9a8629383d2ed0274465cfe0c2f15da25
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-16 04:57:31 -0700

    5more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 10times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int num, count = 0;
+	while (count < 10)
+	{
+	for (num = 0; num <= 14; num++)
+	{
+		if (num > 9)
+			_putchar((num / 10 + '0');
+
+		_putchar((num % 10) + '0');
+	}
+	_putchar('\n');
+	count++;
+	}
+}
