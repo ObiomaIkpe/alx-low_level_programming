89a5a7e70949b8f1cac6aa0f803b7e4018bd0399
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-30 15:59:04 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - multiplies two numbers
+ *
+ *   * @argc: number of arguments
+ *
+ *    * @argv: array of arguments
+ *
+ *     * Return: 0 as success and 1 as error
+ *
+ *      */
+
+
+
+int main(int argc, char **argv)
+
+{
+
+		if (argc != 3)
+
+				{
+
+							printf("Error\n");
+
+									return (1);
+
+										}
+
+			printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+				return (0);
+
+}
