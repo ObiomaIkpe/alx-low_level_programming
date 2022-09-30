aa2c70c5e5487ee9eda72689b2dbfd4bfae2ee56
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-30 15:55:26 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the name of its executable file
+ *
+ *   * @argc: number of input
+ *
+ *    * @argv: array of inputted string
+ *
+ *     * Return: 0 as success
+ *
+ *      */
+
+
+
+int main(int argc, char **argv)
+
+{
+
+		printf("%s\n", argv[0]);
+
+			(void)argc;
+
+				return (0);
+
+}
