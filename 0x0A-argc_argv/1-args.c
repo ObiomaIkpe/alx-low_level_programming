08375c53a06374897ec1c8e657a08d70917e7249
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-09-30 15:37:23 -0700

    task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the number of arguments passed into it
+ *
+ *   * @argc: number of arguments inputted
+ *
+ *    * @argv: array of arguments inputted
+ *
+ *     * Return: 0 as success
+ *
+ *      */
+
+
+
+int main(int argc, char **argv)
+
+{
+
+		(void)argv;
+
+			printf("%d\n", argc - 1);
+
+				return (0);
+
+}
