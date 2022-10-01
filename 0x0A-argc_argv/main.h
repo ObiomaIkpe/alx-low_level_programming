257a0d2ec208badee65847e1c8fd42ad8c4a08ee
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-10-01 00:25:41 -0700

    main

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+#include <stdlib.h>
+
+
+
+int _putchar(char c);
+
+
+
+int main(int argc, char **argv);
+
+
+
+int is_digit(char **argv, int i, int j);
+
+
+
+int change_cent(int *num, int subt, int *change);
