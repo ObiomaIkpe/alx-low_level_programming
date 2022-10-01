041c531d7c5f8deaf87258c5356ad919fdde6d82
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-10-01 01:36:09 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,73 +5,35 @@
 #include <ctype.h>
 
 /**
- *
- *  * main - adds positive numbers with certain exceptions
- *
- *   * @argc: total arguments inputted
- *
- *    * @argv: array of arguments inputted
- *
- *     * Return: 0 if successeful and 1 if not
- *
- *      */
-
-
+ * main - adds positive numbers with certain exceptions
+ * @argc: total arguments inputted
+ * @argv: array of arguments inputted
+ * Return: 0 if successeful and 1 if not
+ */
 
 int main(int argc, char **argv)
-
 {
-
-		int j, i = 1;
-
-			int sum = 0;
-
-				char *s;
-
-
-
-					if (argc == 1)
-
-							{
-
-										printf("0\n");
-
-												return (0);
-
-													}
-
-						while (i < argc)
-
-								{
-
-											s = argv[i];
-
-													for (j = 0; s[j] != '\0'; j++)
-
-																{
-
-																				if (isdigit(*(s + j)) == 0)
-
-																								{
-
-																													printf("Error\n");
-
-																																	return (1);
-
-																																				}
-
-																						}
-
-															i++;
-
-																	sum += atoi(s);
-
-																		}
-
-							printf("%d\n", sum);
-
-								return (0);
-
+	int j, i = 1;
+	int sum = 0;
+	char *s;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		s = argv[i];
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (isdigit(*(s + j)) == 0)
+			{
+			printf("Error\n");
+				return (1);
+			}
+		}										i++;
+		sum += atoi(s);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
-
-
