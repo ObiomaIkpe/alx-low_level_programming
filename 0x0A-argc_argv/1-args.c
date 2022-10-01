0d9b1659ef0a42be4c7cdd072a3da3571eba3de6
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-10-01 00:47:02 -0700

    main

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints the number of arguments passed into it
- * @argc - number of arguments inputted 
- * @argv - array of arguemnts inputted
+ * @argc: number of arguments inputted
+ * @argv: array of arguemnts inputted
  * Return: 0 as sucess
  */
 
@@ -12,5 +12,5 @@ int main(int argc, char **argv)
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
-        return (0);
-}       
+return (0);
+}
