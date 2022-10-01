6018c17b5c8cac70489f09d086555ee25fb37410
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-10-01 01:11:02 -0700

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,41 +1,23 @@
 #include <stdlib.h>
-
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints all inputted arguments
- *
- *   * @argc: number of arguments inputted
- *
- *    * @argv: array of arguments inputted
- *
- *     * Return: 0 as sucess
- *
- *      */
-
-
+ * main - prints all inputted arguments
+ * @argc: number of arguements inputted
+ * @argv: array of arguements inputted
+ * Return: 0 as success
+ */
 
 int main(int argc, char **argv)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (i < argc)
-
-					{
-
-								printf("%s\n", argv[i]);
-
-										i++;
-
-											}
-
-				return (0);
-
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
 }
+
+
