757258e79b3cf7dd425223cd4cd8eadec857e57d
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-10-02 09:48:25 -0700

    mitnick

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_abs - finds absolute value of an integer
+ * @n: argument
+ * Description: Finds absolute value of an integer
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+	return (0);
+}
+
