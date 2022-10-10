c3131eccea551e3c0443e0b70cd8c40ac1f6c97b
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-10-10 03:48:34 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the name of the file it was compiled from,
+ * followed by a new line
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	
+	return (0);
+}
