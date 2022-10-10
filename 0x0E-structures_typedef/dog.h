af36e5ca661ca5475c826c6fd32795a1b218707e
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-10-10 13:17:30 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- *dog_t - new struct dog
+ * dog_t - new struct dog
  */
 
 typedef struct dog dog_t;
@@ -17,16 +17,21 @@ typedef struct dog dog_t;
  */
 
 struct dog
+
 {
 	char *name;
 	float age;
 	char *owner;
-
 };
 
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
 #endif
