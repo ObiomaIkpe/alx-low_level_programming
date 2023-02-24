79fa2d3d9e7b4dad89e34bc3960b89854e71c7cf
ObiomaIkpe <obiomaikpe1.com>
2023-02-24 22:12:32 +0100

    basic recursions

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int square(int n, int i);
+
+int is_prime_number(int n);
+
+int is_n_prime(int n, int i);
+
+int is_palindrome(char *s);
+
+void rev_string(char *r, int i, int _str_len);
+
+int is_palindrome_check(char *s, int f, int l);
+
+int _strlen(char *s);
+
+void swap(char *a, char *b);
+
+int wildcmp(char *s1, char *s2);
