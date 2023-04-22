f772c48695aeb6391e32b981c1970c14873a0008
ObiomaIkpe <obiomaikpe1.com>
2023-04-22 08:32:51 +0100

    print first argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
-
-
 /**
  * main - prints the name of its executable file
- *
  * @argc: number of input
- *
  * @argv: array of inputted string
- *
  * Return: 0 as success
- *
  */
 
 
-int main(int argc __attribute__((unused)), char* argv[]){
-_putchar("%s\n", argv[0]);
-return 0;
+int main(int argc __attribute__((unused)), char* argv[])
+{
+    int j;
+    for (j = 0; argv[0][j] != '\0'; j++)
+	_putchar(argv[0][j];
+    _putchar('\n'):
+
+   return (0);
 }
